e8decce90c1e6a5e1fa0dc2531bf037826855aa6
shuyuwx <158557746+shuyuwx@users.noreply.github.com>
2024-08-03 16:30:17 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,8 +7,10 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm797371572-4540e26c6594782943a7b6cf',
+    'm797376265-5b5dc77a7232483a00b69703',
     'm797371579-eab2bc44b3a39e6519dd8385',
     'm797371620-0a84428fa94077fbb8c78a62',
+    'm797376184-cfa426562a382c3af0bbe5c0',
     'm797371766-1eea205f692edaab1ecfcfbb',
     'm797371668-781e8d7e46ea5191a6882d9c',
     'm797371740-9a210adc021dc6e4389e8694',
